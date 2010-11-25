a1a69f5eb19a74b064437c1ab229d3de158db40d
Palo M <palo-m@users.noreply.github.com>
2010-11-25 16:38:35 +0000

    Initial import of sources

---
+++ src/sk/boinc/androboinc/util/ClientId.java
@@ -0,0 +1,111 @@
+/* 
+ * AndroBOINC - BOINC Manager for Android
+ * Copyright (C) 2010, Pavol Michalec
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+package sk.boinc.androboinc.util;
+
+import android.database.Cursor;
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class ClientId implements Parcelable {
+	public static final String TAG = "ClientId";
+	private long   mId = -1;
+	private String mNickname = null;
+	private String mAddress = null;
+	private int    mPort = 31416;
+	private String mPassword = null;
+
+	@Override
+	public int describeContents() {
+		return 0;
+	}
+
+	@Override
+	public void writeToParcel(Parcel dest, int flags) {
+		dest.writeLong(mId);
+		dest.writeString(mNickname);
+		dest.writeString(mAddress);
+		dest.writeInt(mPort);
+		dest.writeString(mPassword);
+	}
+
+	public static final Parcelable.Creator<ClientId> CREATOR
+		= new Parcelable.Creator<ClientId>() {
+			@Override
+			public ClientId createFromParcel(Parcel in) {
+				return new ClientId(in);
+			}
+
+			@Override
+			public ClientId[] newArray(int size) {
+				return new ClientId[size];
+			}
+	};
+
+	private ClientId(Parcel in) {
+		mId = in.readLong();
+		mNickname = in.readString();
+		mAddress = in.readString();
+		mPort = in.readInt();
+		mPassword = in.readString();
+	}
+
+	public ClientId(long id, String nickname, String address, int port, String password) {
+		mId = id;
+		mNickname = nickname;
+		mAddress = address;
+		mPort = port;
+		mPassword = password;
+	}
+
+	public ClientId(Cursor c) {
+		mId       = c.getLong(c.getColumnIndexOrThrow(HostListDbAdapter.KEY_ROWID));
+		mNickname = c.getString(c.getColumnIndexOrThrow(HostListDbAdapter.FIELD_HOST_NICKNAME));
+		mAddress  = c.getString(c.getColumnIndexOrThrow(HostListDbAdapter.FIELD_HOST_ADDRESS));
+		mPort     = c.getInt(c.getColumnIndexOrThrow(HostListDbAdapter.FIELD_HOST_PORT));
+		mPassword = c.getString(c.getColumnIndexOrThrow(HostListDbAdapter.FIELD_HOST_PASSWORD));
+	}
+
+	public final long getId() {
+		return mId;
+	}
+
+	public final String getNickname() {
+		return mNickname;
+	}
+
+	public final String getAddress() {
+		return mAddress;
+	}
+
+	public final int getPort() {
+		return mPort;
+	}
+
+	public final String getPassword() {
+		return mPassword;
+	}
+
+	public void updateHost(String nickname, String address, int port, String password) {
+		mNickname = nickname;
+		mAddress = address;
+		mPort = port;
+		mPassword = password;
+	}
+}
