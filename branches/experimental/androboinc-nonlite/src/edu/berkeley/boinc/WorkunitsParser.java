df17175826a1fbe56e76a62e9ee25f815f29d8f0
pavol.michalec@gmail.com <pavol.michalec@gmail.com@6fa3d978-bf49-d8fa-b4a4-7201f5faeffe>
2012-08-26 13:24:48 +0000

    Copyright of classes in packages edu.berkeley.boinc and edu.berkeley.boinc.util transferred to University of California (same as original BOINC). Copyright of other classes (including edu.berkeley.boinc.lite) remains the same as before.

--- branches/experimental/androboinc-nonlite/src/edu/berkeley/boinc/WorkunitsParser.java
+++ branches/experimental/androboinc-nonlite/src/edu/berkeley/boinc/WorkunitsParser.java
@@ -1,6 +1,5 @@
 /* 
- * AndroBOINC - BOINC Manager for Android
- * Copyright (C) 2010, Pavol Michalec
+ * Copyright (C) 2012 University of California
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
